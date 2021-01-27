ecd7e93be8e5aedf845f3a1715d36fd2b72be74e
abel <aludena18@gmail.com>
2021-01-26 19:57:34 -0500

    upd server lines

--- udpServer.js
+++ udpServer.js
@@ -1,12 +1,14 @@
 console.log("CALAMP UDP SERVER TEST");
 
-const dgram = require('dgram');
-const socket = dgram.createSocket('udp4');
-
-socket.on('message', (msg, rinfo) => {
-  //console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-  console.log('Recibido = ' + msg);
-  console.log('Desde = ' + rinfo.address + ":" + rinfo.port);
-});
+const dgram = require("dgram");
+const socket = dgram.createSocket("udp4");
 
 socket.bind(31000);
+
+while (true) {
+  socket.on("message", (msg, rinfo) => {
+    //console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    console.log("Recibido = " + msg);
+    console.log("Desde = " + rinfo.address + ":" + rinfo.port);
+  });
+}
