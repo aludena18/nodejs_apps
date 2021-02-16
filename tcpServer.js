462bc92f84a92c40c8c7d8d0712e48aa97cfcbd2
abel <aludena18@gmail.com>
2021-02-16 15:34:22 -0500

    16022021

--- tcpServer.js
+++ tcpServer.js
@@ -16,14 +16,14 @@ server.on('connection',(socket)=>{
     console.log(`\n${new Date()} (${arrByte.length} bytes) : ${dataHex}`)
     
     if(data[13]==1){
-      socket.pipe(socket)
+      //socket.pipe(socket)
       socket.write(ack(data),()=>{
         console.log('ACK Eviado!!')
       })
     }
 
     if(data[13]==2){
-      socket.unpipe(socket);
+      //socket.unpipe(socket);
     }
     
   })
