85e0e4c8869144290dec064ae0480d699eca5eea
abel <aludena18@gmail.com>
2021-03-11 01:02:36 -0500

    11032021

--- tcpServer.js
+++ tcpServer.js
@@ -3,26 +3,6 @@ let net = require("net");
 //Puerto TCP
 const TCP_SERVER_PORT = 50115;
 
-//Tipo de mensajes del protocolo Calamp
-const MSG_NULL = 0;
-const MSG_ACK = 1;
-const MSG_EVENT_REPORT = 2;
-const MSG_ID_REPORT = 3;
-const MSG_USER_DATA = 4;
-const MSG_APP_DATA = 5;
-const MSG_CONFIG = 6;
-const MSG_UNIT_REQUEST = 7;
-const MSG_LOCATE_REPORT = 8;
-const MSG_USER_DATA_ACC = 9;
-const MSG_MINI_EVENT_REPORT = 10;
-const MSG_MINI_USER_DATA = 11;
-
-//Tipo de servicios del protocolo Calamp
-const SERVICE_UNACKNOWLEDGED = 0;
-const SERVICE_ACKNOWLEDGED = 1;
-const SERVICE_RESPONSE = 2;
-
-
 let server = net.createServer();
 server.listen(TCP_SERVER_PORT, () => {
   console.log("Servidor escuchando a %j", server.address());
@@ -36,9 +16,6 @@ server.on("connection", (socket) => {
     //let dataHex = data.toString('hex');
     let dataHex = String2Hex(data);
     console.log(`\n${new Date()} (${data.length} bytes) : ${dataHex}`);
-    
-    processData(socket,data);
-
   });
 
   socket.on("drain", () => {
@@ -58,44 +35,10 @@ server.on("connection", (socket) => {
   });
 });
 
-//decodifica mensaje recibido
-function processData(skt, data){
-
-  const header = data.subarray(0,2);
-  const optionsHeader = data.subarray(4,13);
-  const srvType = data.subarray(13,14);
-  const msgType = data.subarray(14,15);
-  const seqNumber = data.subarray(15,17);
-
-  if(srvType.readInt8(0)==SERVICE_ACKNOWLEDGED){
-    let res = response(header,optionsHeader,seqNumber,msgType);
-    skt.write(res);
-  }
-
-  
-}
-
-function response(header,opHeader,seq,type) {
-  const srvType = Buffer.alloc(1,SERVICE_RESPONSE);
-  const msgType = Buffer.alloc(1,MSG_ACK);
-  const ackMsg = Buffer.alloc(5,0);
-
-  let rawDataLenInt = opHeader.length + srvType.length + msgType.length + seq.length + type.length + ackMsg.length;
-  const rawDataLenBuff = Buffer.alloc(2,0);
-  rawDataLenBuff.writeInt8(rawDataLenInt,1);
-  
-  let dataLen = header.length + rawDataLenBuff.length + rawDataLenInt;
-
-  const dataBuff = Buffer.concat([header,rawDataLenBuff,opHeader,srvType,msgType,seq,type,ackMsg],dataLen);
-  console.log(dataBuff)
-  return dataBuff;
-}
-
 function String2Hex(tmp) {
   let str = "";
   for (let i = 0; i < tmp.length; i++) {
     str += tmp[i].toString(16) + " ";
   }
   return str;
-  
 }
---
+++ udpServer_Calamp.js
@@ -0,0 +1,54 @@
+let port = 50115;
+let buff = [];
+
+const dgram = require("dgram");
+const socket = dgram.createSocket("udp4");
+
+//RECIBE MENSAJES DEL CLIENTE
+socket.on("message", (msg, rinfo) => {
+    buff = msg;
+    console.log(`server got ${buff.length} bytes : ${String2Hex(buff)} / from ${rinfo.address}:${rinfo.port} Evnt:${buff[50]}`);
+    sendAck(buff, rinfo);
+});
+
+function sendAck(data,info){
+    let ackArray=[
+        data[0],
+        data[1],
+        data[2],data[3],data[4],data[5],data[6],
+        data[7],
+        data[8],
+        2,
+        1,
+        data[11],data[12],
+        data[10],
+        0,
+        0,
+        0,0,0
+    ]
+
+    sendMsg(Buffer.from(ackArray),info);
+}
+
+function sendMsg(msg,info){
+    socket.send(msg, info.port, info.address);
+}
+
+function parseFunction(data,i,f){
+    let str = '';
+    for(let index=i ; index<f ; index++){
+        str += data[index].toString(16) + " ";
+    }
+    return str
+}
+
+function String2Hex(tmp) {
+    let str = '';
+    for(let i = 0; i < tmp.length; i++) {
+        str += tmp[i].toString(16) + " ";
+    }
+    return str;
+}
+
+socket.bind(port);
+console.log ("Servidor UDP iniciado en el puerto " + port);
